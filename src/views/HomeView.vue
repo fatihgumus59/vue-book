7be2fdca07c96f2adc77c674b7140d72517f7b3e
Fatih Gumus <fatihgumus72@gmail.com>
2024-08-14 22:12:43 +0300

    book listed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,25 @@
 <template>
-    <div>
+    <div class="container">
         <SectionHeader :title="title"  description="Anasayfaya Hoşgeldiniz!"/>
+        <BookList :books="books"/>
     </div>
 </template>
 
 <script>
     import SectionHeader from '@/components/SectionHeader.vue';
+    import BookList from '@/components/BookList.vue';
+    import books from '@/db';
 
     export default {
         name : "HomeView",
         components:{
-            SectionHeader
+            SectionHeader,
+            BookList
         },
         data(){
             return{
-                title :'Anasayfa'
+                title :'Anasayfa',
+                books : books
             }
         }
     }
