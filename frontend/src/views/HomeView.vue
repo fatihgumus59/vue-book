176f2d1fe737af0992b032ef4982a366f15a9b32
Fatih Gumus <fatihgumus72@gmail.com>
2024-10-08 21:42:50 +0300

    Carousel page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,24 +1,31 @@
 <template>
-    <section class="full-section-height">
+    <section>
+        <Carousel :items="carouselItems">
 
-        <div class="container">
-            <SectionHeader :title="title" description="Anasayfaya Hoşgeldiniz!" />
-
-        </div>
+        </Carousel>
     </section>
 </template>
 
 <script>
-import SectionHeader from '@/components/SectionHeader.vue';
+import Carousel from '@/components/widgets/Carousel.vue';
+import hero_1 from '@/assets/images/hero_1.jpg';
+import hero_2 from '@/assets/images/hero_2.jpg';
+import hero_3 from '@/assets/images/hero_3.jpg';
+
 
 export default {
     name: "HomeView",
     components: {
-        SectionHeader,
+        Carousel,
     },
     data() {
         return {
             title: 'Anasayfa',
+            carouselItems: [
+                { imageUrl: hero_1, subtitle: 'Liberte', title: 'Lorem Ipsum Dolor Sit Amet', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+                { imageUrl: hero_2, subtitle: 'Egalite', title: 'Excepteur Sint Occaecat Cupidatat', description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+                { imageUrl: hero_3, subtitle: 'Fraternite', title: 'Neque Porro Quisquam Est', description: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.' }
+            ],
         }
     }
 }
